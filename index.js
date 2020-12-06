04840bdb684ad01a245058286bc63c733080bc8e
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-06 15:05:55 +0100

    add test data and pseudo code

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+let edges = [
+    ['14th&6th', '23rd&6th'],
+    ['23rd&6th', '34th&6th'],
+    ['34th&6th', '28th&Bwy'],
+    ['28th&Bwy', '23rd&Bwy'],
+    ['23rd&Bwy', '14th&Lex'],
+    ['14th&Lex', '23rd&Lex']
+]
+ 
+let vertices = [
+  {name: '34th&6th', distance: null, predecessor: null},
+  {name: '23rd&6th', distance: null, predecessor: null},
+  {name: '14th&6th', distance: null, predecessor: null},
+  {name: '28th&Bwy', distance: null, predecessor: null},
+  {name: '23rd&Bwy', distance: null, predecessor: null},
+  {name: '14th&Lex', distance: null, predecessor: null},
+  {name: '23rd&Lex', distance: null, predecessor: null},
+]
+
+/*
+    rootNode = vertices[0]
+    queue = []
+    addVertexToQueue(rootNode)
+        // queue = [rootNode]
+    while(!queue.length == 0) {
+        let firstNode = queue.shift()
+    adjacentVertices = findAdjacent(firstNode)
+        for vertex in adjacentVertices {
+            markDistanceAndPredecessor(vertex)
+            addToQueue(vertex)
+        }
+    }
+
+*/
+
 function bfs(rootNode, vertices, edges){
 
 }
