62b6e62476e4a4ab22e61767c117c550cc05c552
jessicagomesng <jgnpeluso@gmail.com>
2021-01-08 15:59:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,75 +1,31 @@
-function bfs(startingNode, vertices, edges){
-    startingNode.distance = 0
-    let discovered = [startingNode]
-    let discoverOrder = [startingNode]
-    while(discovered.length != 0){
-      let currentNode = discovered.shift()
-      let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
-      discoverOrder = discoverOrder.concat(adjacentNodes);
-      markDistanceAndPredecessor(currentNode, adjacentNodes)
-      discovered = discovered.concat(adjacentNodes)
-    }
-    return discoverOrder
-  }
+function findAdjacent(nodeName, vertices, edges) {
+  let adjacentNames = edges.filter( (edge) => 
+    edge.includes(nodeName)
+  ).map( (edge) => edge.find( (name) => name !== nodeName) )
 
-// function findVertice(name, vertices) {
-//     let currentVertice = vertices[0] 
-//     let newVertices = vertices.slice(1)
-    
-//     if (currentVertice.name === name) {
-//         return currentVertice;
+  let adjacentVertices = adjacentNames.map( (adjacent) => vertices.find( (vertice) => vertice.name === adjacent) )
 
-//     } else {
-//         currentVertice = newVertices[0]
-//         return findVertice(name, newVertices)
-//     }
-// }
+  return  adjacentVertices.filter( (vertice) => vertice.distance === null )
 
-function findVertice(nodeName, vertices) {
-    return vertices.find((vertice) => vertice.name === nodeName)
 }
-
-// function findAdjacent(nodeName, vertices, edges) {
-//     let currentEdge = edges[0];
-//     let adjacentEdge = edges[1];
-//     let adjacentNodes = [];
-
-//     if (currentEdge[0] === node) {
-//         adjacentNodes.push(findVertice(currentEdge[1], vertices));
-//     } else if (currentEdge[1] === node) {
-//         adjacentNodes.push(findVertice(currentEdge[0], vertices));
-//         adjacentNodes.push(findVertice(adjacentEdge[1], vertices))
-//     } else {
-//         let newEdges = edges.slice(1);
-//         return findAdjacent(node, vertices, newEdges)
-//     }
-
-//     let filteredNodes = adjacentNodes.filter((node) => node.distance === null )
-
-//     return filteredNodes;
-// }
-
-
-function findAdjacent(nodeName,  vertices, edges){
-    return edges.filter(function(edge){
-      return edge.includes(nodeName)
-    }).map(function(edge){
-      return edge.filter(function(node){
-        return (node != nodeName)
-      })[0]
-    }).map(function(name){
-      return findVertice(name, vertices)
-    }).filter(function(node){
-      return node.distance == null;
-    })
-  }
 
 function markDistanceAndPredecessor(predecessor, adjacentNodes) {
-    for (const adjacent of adjacentNodes) {
-        adjacent.distance = predecessor.distance + 1;
-        adjacent.predecessor = predecessor;
-    }
-
-    return adjacentNodes;
+  adjacentNodes.map( (node) => { node.predecessor = predecessor; node.distance = predecessor.distance + 1 })
 }
 
+function bfs(startingNode, vertices, edges) {
+  startingNode.distance = 0;
+  let queue = [startingNode];
+  let discovered = [startingNode];
+  // mark startingNode's distance as zero
+  // want to repeat from here
+  while (queue.length !== 0) {
+    let currentNode = queue.shift();
+    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges);
+    markDistanceAndPredecessor(currentNode, adjacentNodes);
+    adjacentNodes.map( (node) => queue.push(node) );
+    discovered = discovered.concat(adjacentNodes);
+  }
+
+  return discovered;
+}
