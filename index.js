904102c8d7a7c3c6a6b077500eb9ac21e9e418d6
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-18 21:24:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+rootNode = vertices[0]
+    queue = []
+    addVertexToQueue(rootNode)
+        // queue = [rootNode]
+    while(!queue.length == 0) {
+        let firstNode = queue.shift()
+    adjacentVertices = findAdjacent(firstNode)
+        for vertex in adjacentVertices {
+            markDistanceAndPredecessor(vertex)
+            addToQueue(vertex)
+        }
+    }
+    
